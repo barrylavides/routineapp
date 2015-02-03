960fe943abc6f6e55dfa00483d626d7526ccb175
Barry <lavidesbarry@gmail.com>
2015-02-04 04:47:24 +0800

    database connection test

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask.ext.pymongo import PyMongo
+from pymongo import MongoClient
+
 app = Flask(__name__)
+uri = 'mongodb://localhost/routineapp'
+c = MongoClient(uri)
+db = c.get_default_database()
 
 @app.route('/')
 def index():
+    r = list(db.yo.find())
+
+    print list(r)
+
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
