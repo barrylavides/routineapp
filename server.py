7b292f59e4b3aac3c45f5b4d1e2ee5a7b9b00f2f
Barry <lavidesbarry@gmail.com>
2015-02-04 05:20:43 +0800

    api for getting all tasks

--- server.py
+++ server.py
@@ -1,19 +1,36 @@
-from flask import Flask
+from flask import Flask, url_for, render_template, jsonify
 from flask.ext.pymongo import PyMongo
 from pymongo import MongoClient
+from bson.json_util import dumps
+import ast
 
 app = Flask(__name__)
 uri = 'mongodb://localhost/routineapp'
 c = MongoClient(uri)
 db = c.get_default_database()
 
+def mongoObj(jsn):
+    jsn = ast.literal_eval(dumps(jsn))
+    
+    # mongo object returns BJSON as its id
+    # this function will convert the BSJON to json
+    # and at the same time will remove the oid,
+    # and assign the oid's value to its parent id
+    for i,v in enumerate(jsn):
+        jsn[i]['_id'] = jsn[i]['_id']['$oid']
+    
+    return jsn
+
+
 @app.route('/')
 def index():
-    r = list(db.yo.find())
+    return render_template('index.html')
 
-    print list(r)
-
-    return 'Hello World'
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    r = mongoObj(list(db.task.find()))
+    
+    return jsonify({'tasks': r})
 
 if __name__ == '__main__':
     app.run(debug=True)
